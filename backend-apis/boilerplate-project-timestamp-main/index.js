3064ec95d04b3b48a1a9b7d2402a2c939ca3e457
haley <haley.elder@live.com>
2024-09-30 13:47:39 -0700

    update source code backend - timestamp ref

--- backend-apis/boilerplate-project-timestamp-main/index.js
+++ backend-apis/boilerplate-project-timestamp-main/index.js
@@ -10,6 +10,16 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+
 app.get('/api/:date?', function(req,res) {
   let dateQuery  = req.params.date
   let unix;
@@ -44,6 +54,8 @@ app.get('/api/:date?', function(req,res) {
 
   res.json({'unix': unix, "utc": UTCtime})
 })
+
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
